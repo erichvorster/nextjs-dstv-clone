2e9660452ce0bc684c3fe1b2c563a8924ae4139f
erichvorster <erichvorster1@gmail.com>
2022-07-16 13:33:55 +0200

    develop v1

---
+++ components/Nav.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+
+import requests from "../utilities/requests";
+import dstv from "../public/DStv.svg";
+
+const Nav = () => {
+  const router = useRouter();
+
+  return (
+    <nav>
+      <div className="pt-2 w-24 px-2">
+        <Image src={dstv} height={30} width={80} />
+      </div>
+      <div className="relative">
+        <div className="flex px-2 sm:px-5  text-1xl whitespace-nowrap space-x-5 sm:space-x-20 overflow-x-scroll scrollbar-hide">
+          {Object.entries(requests).map(([key, { title, url }]) => (
+            <>
+              <h2
+                key={key}
+                onClick={() => router.push(`/?genre=${key}`)}
+                className="last:pr-24 cursor-pointer transition duration-100 transform  hover:text-sky-500 active:text-sky-500"
+              >
+                {title}
+              </h2>
+            </>
+          ))}
+        </div>
+        <div className="absolute top-0 right-0 bg-gradient-to-l from-black to h-10 w-1/12" />
+        </div>
+    </nav>
+  );
+};
+
+export default Nav;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
 import React from "react";
 import Head from "next/head";
+import Nav from "../components/Nav";
 
-export default function Home() {
+const API_KEY = process.env.API_KEY;
+
+export const getStaticProps = async () => {
+  const res = await fetch(`
+  https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`);
+  const data = await res.json()
+  
+  return {
+    props: {hero:data}
+  }
+}
+
+
+export default function Home({hero}) {
+  console.log(hero)
   return (
+    <>
     <Head>
       <title>DSTV</title>
     </Head>
+
+    <Nav/>
+    </>
   );
 }
+
